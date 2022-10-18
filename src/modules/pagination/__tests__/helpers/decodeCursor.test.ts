74bb525847e0a4418a6fc3c3c08628d43fee71b5
rconrardy <conrardy@umich.edu>
2022-10-18 01:14:33 -0400

    Add invalid cursor error checking to decode cursor method.

--- src/modules/pagination/__tests__/helpers/decodeCursor.test.ts
+++ src/modules/pagination/__tests__/helpers/decodeCursor.test.ts
@@ -1,16 +1,89 @@
+import { DecodedCursor } from '@jest-games-organization/backend-package-graphql-types';
 import { encodeObject } from '@jest-games-organization/backend-package-object-helpers';
 import { decodeCursor } from '../../helpers/decodeCursor';
 
 describe('GIVEN the decodeCursor method', () => {
   let cursor: string;
 
-  beforeEach(() => {
-    cursor = encodeObject({ data: {}, args: {} });
+  describe('WHEN the cursor is not valid', () => {
+    describe('WHEN there are no order by keys', () => {
+      let decodedCursor: DecodedCursor<{}>;
+
+      beforeEach(() => {
+        decodedCursor = { args: { orderBy: [] }, data: {} };
+        cursor = encodeObject(decodedCursor);
+      });
+
+      test('THEN it should throw an error', () => {
+        expect(() => decodeCursor(cursor)).toThrowError('The cursor is invalid: order by keys not found.');
+      });
+    });
+
+    describe('WHEN there are no data keys', () => {
+      let decodedCursor: DecodedCursor<{}>;
+
+      beforeEach(() => {
+        decodedCursor = { args: { orderBy: [{ id: 'mockId' }] }, data: {} };
+        cursor = encodeObject(decodedCursor);
+      });
+
+      test('THEN it should throw an error', () => {
+        expect(() => decodeCursor(cursor)).toThrowError('The cursor is invalid: data keys not found.');
+      });
+    });
+
+    describe('WHEN the order by keys length and data keys length are not the same', () => {
+      let decodedCursor: DecodedCursor<{ id: string; a?: string }>;
+
+      beforeEach(() => {
+        decodedCursor = {
+          data: { id: 'mockId' },
+          args: { orderBy: [{ id: 'asc' }, { a: 'desc' }] },
+        };
+        cursor = encodeObject(decodedCursor);
+      });
+
+      test('THEN it should throw an error', () => {
+        expect(() => decodeCursor(cursor)).toThrowError(
+          'The cursor is invalid: the number of order by and data keys do not match.',
+        );
+      });
+    });
+
+    describe('WHEN the order by keys and data keys are not the same', () => {
+      let decodedCursor: DecodedCursor<{ id: string; a?: string; b?: string }>;
+
+      beforeEach(() => {
+        decodedCursor = {
+          data: { id: 'mockId', a: 'mockA' },
+          args: { orderBy: [{ id: 'asc' }, { b: 'desc' }] },
+        };
+        cursor = encodeObject(decodedCursor);
+      });
+
+      test('THEN it should throw an error', () => {
+        expect(() => decodeCursor(cursor)).toThrowError(
+          'The cursor is invalid: the order by and data keys do not match.',
+        );
+      });
+    });
   });
 
-  test('THEN it should return the decoded cursor', () => {
-    const response = decodeCursor(cursor);
-    const expected = { data: {}, args: {} };
-    expect(response).toEqual(expected);
+  describe('WHEN the cursor is valid', () => {
+    let decodedCursor: DecodedCursor<{ id: string }>;
+
+    beforeEach(() => {
+      decodedCursor = {
+        data: { id: 'mockId' },
+        args: { orderBy: [{ id: 'asc' }] },
+      };
+      cursor = encodeObject(decodedCursor);
+    });
+
+    test('THEN it should return the decoded cursor', () => {
+      const response = decodeCursor(cursor);
+      const expected = decodedCursor;
+      expect(response).toEqual(expected);
+    });
   });
 });
--- src/modules/pagination/helpers/decodeCursor.ts
+++ src/modules/pagination/helpers/decodeCursor.ts
@@ -7,6 +7,38 @@ import { decodeObject } from '@jest-games-organization/backend-package-object-he
  * @returns The decoded cursor.
  */
 export const decodeCursor = <Record extends { [key: string]: unknown }>(cursor: string): DecodedCursor<Record> => {
+  // Decode the cursor.
+  const { args, data } = decodeObject<DecodedCursor<Record>>(cursor);
+
+  // Define the Key type.
+  type Key = keyof Record;
+
+  // Get the order by keys.
+  const orderByKeys: Key[] = args.orderBy.map((o) => Object.keys(o)[0]).sort();
+
+  // Get the data keys.
+  const dataKeys: Key[] = Object.keys(data).sort();
+
+  // Check if there are order by keys, if not throw an error.
+  if (orderByKeys.length === 0) {
+    throw new Error('The cursor is invalid: order by keys not found.');
+  }
+
+  // Check if there are data keys, if not throw an error.
+  if (dataKeys.length === 0) {
+    throw new Error('The cursor is invalid: data keys not found.');
+  }
+
+  // Check if the order by keys length and data keys length are the same, if not throw an error.
+  if (orderByKeys.length !== dataKeys.length) {
+    throw new Error('The cursor is invalid: the number of order by and data keys do not match.');
+  }
+
+  // Check if the order by keys and date keys are the same, if not throw an error.
+  if (orderByKeys.join(',') !== dataKeys.join(',')) {
+    throw new Error('The cursor is invalid: the order by and data keys do not match.');
+  }
+
   // Return the decoded cursor.
-  return decodeObject<DecodedCursor<Record>>(cursor);
+  return { args, data };
 };
